28d42f4d39d4371cbdfebdd861f8fc6189a049c4
jeong jun <jungjun3047@naver.com>
2024-04-05 01:36:53 +0900

    feat: data 도구 추가

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,7 +1,14 @@
 import { RecoilRoot } from "recoil";
 import StackLayout from "./stackLayout";
+import * as amplitude from '@amplitude/analytics-react-native';
+import { useEffect } from "react";
 
   export default () => {
+
+    useEffect(() => {
+      amplitude.init('e66caa8537e664f131e86994b19cae12');
+      amplitude.track('test: 시작');
+    }, []);
 
     return (
         <RecoilRoot>
