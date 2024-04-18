1964915ed43b79eaf1bcbc1303c5b6caf39dad1b
jeong jun <jungjun3047@naver.com>
2024-04-18 17:56:05 +0900

    feat: eas update 가능하게 추가

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,11 +2,26 @@ import { RecoilRoot } from "recoil";
 import StackLayout from "./stackLayout";
 import * as amplitude from '@amplitude/analytics-react-native';
 import { useEffect } from "react";
+import * as Updates from 'expo-updates';
 
   export default () => {
 
+    async function onFetchUpdateAsync() {
+      try {
+        const update = await Updates.checkForUpdateAsync();
+  
+        if (update.isAvailable) {
+          await Updates.fetchUpdateAsync();
+          await Updates.reloadAsync();
+        }
+      } catch (error) {
+        alert(`Error fetching latest Expo update: ${error}`);
+      }
+    }  
+
     useEffect(() => {
-      amplitude.init('e66caa8537e664f131e86994b19cae12');
+      amplitude.init(process.env.EXPO_PUBLIC_AMPLITUDE_KEY);
+      onFetchUpdateAsync()
     }, []);
 
     return (
